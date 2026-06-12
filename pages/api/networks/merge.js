5584a46047b59a3c6e7016d7cf53fdcbee0c2ed3
smitselos <smitselos@gmail.com>
2026-06-13 01:04:17 +0300

    Add files via upload

--- pages/api/networks/merge.js
+++ pages/api/networks/merge.js
@@ -95,7 +95,7 @@ export default async function handler(req, res) {
 
       const pageWidth = 595;
       const pageHeight = 842;
-      const margin = 60;
+      const margin = 28;
       const lineHeight = 14;
       const maxWidth = pageWidth - margin * 2;
 
