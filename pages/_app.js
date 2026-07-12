a13f00991778ca5619c35f476cffdbc683cdf858
smitselos <smitselos@gmail.com>
2026-07-12 22:36:48 +0300

    Add files via upload

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
 // pages/_app.js
+import { useEffect } from 'react';
 import { SessionProvider } from 'next-auth/react';
 
 export default function App({ Component, pageProps: { session, ...pageProps } }) {
+  // Εγγραφή service worker — απαραίτητο για το install prompt σε Android/Chrome
+  useEffect(() => {
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker.register('/sw.js').catch(() => {});
+    }
+  }, []);
+
   return (
     <SessionProvider session={session}>
       <Component {...pageProps} />
